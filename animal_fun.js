8b4c69aa33ffc94001af04e0bef7a17ee76fd145
Joey Jirasevijinda <joey.jira@gmail.com>
2017-10-04 21:51:08 -0700

    sent response to local host

--- animal_fun.js
+++ animal_fun.js
@@ -1,20 +1,31 @@
 const fs = require("fs");
+const http = require("http");
 
-fs.readFile("./animals.txt", "utf-8", (err, data) => {
-  let letter = process.argv[2];
-  let animals_array = data.split("\n");
-  let letter_animals = animals_array.filter((animal) => {
-      return animal[0] === letter.toUpperCase();
-  }).join('\n');
-  if (err) {
-    console.log(err);
-    return;
-  }
-  fs.writeFile(`./${letter}_animals.txt`, letter_animals, err => {
-    if (err) {
-      console.log(err);
-    } else {
-      console.log(letter_animals);
-    }
-  });
-});
+const server = http.createServer((req, res) => {
+  fs.readFile("./animals.txt", "utf-8", (err, data) => {
+    const animalObject = data.split('\n');
+    const selectAnimal = animalObject.filter((animal) => animal[0].toLowerCase() === process.argv[2]);
+    res.write(selectAnimal.join('\n'));
+    res.end();
+  })
+})
+
+server.listen(8000, () => console.log("Listening on port 8000!"))
+// fs.readFile("./animals.txt", "utf-8", (err, data) => {
+//   let letter = process.argv[2];
+//   let animals_array = data.split("\n");
+//   let letter_animals = animals_array.filter((animal) => {
+//       return animal[0] === letter.toUpperCase();
+//   }).join('\n');
+//   if (err) {
+//     console.log(err);
+//     return;
+//   }
+//   fs.writeFile(`./${letter}_animals.txt`, letter_animals, err => {
+//     if (err) {
+//       console.log(err);
+//     } else {
+//       console.log(letter_animals);
+//     }
+//   });
+// });
