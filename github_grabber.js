7756c05fbb05bd081ee38750354ce78b965ec962
Joey Jirasevijinda <joey.jira@gmail.com>
2017-10-04 21:54:22 -0700

    added gitignore

--- github_grabber.js
+++ github_grabber.js
@@ -1,1 +1,10 @@
 const http = require('http');
+
+const githubServer = http.createServer((req, res) => {
+    if (req.method === 'POST') {
+        res.end("I'm a POST request!")
+    }
+    res.end("Danger, not a POST request!")
+})
+
+githubServer.listen(8080, () => console.log('Listening on 8080'))
